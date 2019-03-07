7ec8abfcffc8bbb310fab0557be7162a0479b10b
RichyWalter <richardwalter@web.de>
2019-03-07 14:10:48 +0100

    comments because of build errors

--- java/multithreading_java.java
+++ java/multithreading_java.java
@@ -1,16 +1,16 @@
 //Multithreading with Java from http://openbook.rheinwerk-verlag.de/javainsel9/javainsel_14_002.htm
 
-public class multithreading_java implements Runnable {
+// public class multithreading_java implements Runnable {
 
-    public static void main(String[] args) {
-        System.out.println("Multithreading with Java");
+//     public static void main(String[] args) {
+//         System.out.println("Multithreading with Java");
 
-        Thread t1 = new Thread( new DateCommand() );
-        t1.start();
+//         Thread t1 = new Thread( new DateCommand() );
+//         t1.start();
 
-        Thread t2 = new Thread( new CounterCommand() );
-        t2.start();
+//         Thread t2 = new Thread( new CounterCommand() );
+//         t2.start();
 
-    }   
+//     }   
 
-}
+// }
